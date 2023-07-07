c2fd381636df18bef90c079c16073ac4101e34dc
Junha <junha.ahn.dev@gmail.com>
2023-07-07 14:43:14 +0000

    chore: add aws configure command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,3 +7,7 @@ sudo ./aws/install
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
+
+# aws configure
+# cat ~/.aws/credentials
+# aws sts get-caller-identity
