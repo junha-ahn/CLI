57f727504c212394ab38193873dc372b8169c781
Junha <junha.ahn.dev@gmail.com>
2023-07-07 14:21:48 +0000

    add: preCreate hook for install awscli + terraform

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# https://developer.hashicorp.com/terraform/downloads
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
